7a3daa48b36611b5ae958323aa19c70820ed2f63
vitalik <sandwich.htc@gmail.com>
2023-03-01 16:40:04 +0300

    Added model Reviews, Comment, added CommentSerializer and CommentViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
+from reviews.models import Comment
 from .permissions import AuthorOrReadOnly
-from .serializers import CommentSerializer, ReviewsSerializer
-from reviews.models import Comment, Reviews
+from .serializers import CommentSerializer
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (AuthorOrReadOnly,)
 
     def perform_create(self, serializer):
+        print(self.request.user)
         serializer.save(author=self.request.user)
-    
+
     def get_queryset(self):
-        review_id = self.kwargs.get('id')
+        review_id = self.kwargs.get('review_id')
         return Comment.objects.filter(review=review_id)
