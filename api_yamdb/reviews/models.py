338b1202f13ede310877bf7841e5f2eac361c3c3
vitalik <sandwich.htc@gmail.com>
2023-03-07 14:29:07 +0300

    bag_fix

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,11 +1,8 @@
-from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models import UniqueConstraint
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 from users.models import User
-
-User = get_user_model()
 
 
 class Category(models.Model):
