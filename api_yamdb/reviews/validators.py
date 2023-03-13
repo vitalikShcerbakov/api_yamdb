56064cf6718746a8e74efc4124f1527a5d256c0a
vitalik <sandwich.htc@gmail.com>
2023-03-13 18:46:41 +0300

    Ok ver2

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,11 +1,19 @@
 import re
+from datetime import datetime as dt
 
 from django.core.exceptions import ValidationError
 
 
 def validate_username(name):
     regex_username = re.compile(r'^[\w.@+-]+')
-    if name == 'me':
+    if name.lower() == 'me':
         raise ValidationError('Недопустимое имя "me". Придумайте другое имя.')
     if not regex_username.fullmatch(name):
         raise ValidationError('Letters, digits and @/./+/-/_ only.')
+
+def year_validator(value):
+    if value > int(dt.now().year):
+        raise ValidationError(
+            _('%(value)s is not a correcrt year!'),
+            params={'value': value},
+        )
