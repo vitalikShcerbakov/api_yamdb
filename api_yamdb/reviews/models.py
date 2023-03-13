efb0bf5ca6e155a2fb87431b5d8a1cddf38c6718
LinaLinelle <116231300+LinaLinelle@users.noreply.github.com>
2023-03-14 00:08:21 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,8 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
-from users.models import User
-from .validators import year_validator
 
+from users.models import User
 from .validators import year_validator
 
 
